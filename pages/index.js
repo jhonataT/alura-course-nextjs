1403faeefc044785664409d529a64f4f5f0eac58
JhonataT <jhonata.tenorio2526@gmail.com>
2022-03-09 23:40:54 -0300

    Chapter 3

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
+import React from 'react';
 import Link from "../src/components/Link";
+
+function Title({ children, as }) {
+    const Tag = as;
+    
+    return (
+        <React.Fragment>
+            <Tag>
+                {children}
+            </Tag>
+            <style jsx>{`
+                ${Tag} {
+                    font-family: sans-serif;
+                    color: red;
+                }    
+                
+            `}</style>
+        </React.Fragment>
+    )
+}
 
 export default function HomePage() {
     return (
         <div>
-            <h1>Alura Cases - Home Page</h1>
+            <Title as="h1">Alura Cases - Home Page</Title>
             <Link href='/faq' label='Ir para FAQ' />
         </div>
     )
