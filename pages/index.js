5bba397c2b711f2f745ab74e3f9df28818ba2e8e
JhonataT <jhonata.tenorio2526@gmail.com>
2022-03-12 13:11:28 -0300

    Project struct

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,3 @@
-import React from 'react';
-import Link from "../src/components/Link";
+import HomeScreen from '../src/screens/HomeScreen';
 
-function Title({ children, as }) {
-    const Tag = as;
-    
-    return (
-        <React.Fragment>
-            <Tag>
-                {children}
-            </Tag>
-            <style jsx>{`
-                ${Tag} {
-                    font-family: sans-serif;
-                    color: red;
-                }    
-                
-            `}</style>
-        </React.Fragment>
-    )
-}
-
-export default function HomePage() {
-    return (
-        <div>
-            <Title as="h1">Alura Cases - Home Page</Title>
-            <Link href='/faq' label='Ir para FAQ' />
-        </div>
-    )
-}
+export default HomeScreen;
