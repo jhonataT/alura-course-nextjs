af12306ae3ae13e355ce24d84872474d12c6572d
JhonataT <jhonata.tenorio2526@gmail.com>
2022-03-26 12:56:32 -0300

    Differences between SSG, SSR and ISG

--- pages/faq.js
+++ pages/faq.js
@@ -1,6 +1,16 @@
-import { useEffect, useState } from "react"
+
+// SSG - Static Site Generation;
+// SSR - Server Side Rendering;
+// ISG - Incremental Static Generation
+
+// export async function getServerSideProps {
+//     console.log('Em modo DEV, sempre roda a cada acesso.');
+//     console.log('Rodando a cada acesso que o site recebe.');
+// }
 
 export async function getStaticProps(context) {
+    console.log('Em modo DEV, sempre roda a cada acesso.');
+
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
     const faq = await fetch(FAQ_API_URL)
     .then( result => {
